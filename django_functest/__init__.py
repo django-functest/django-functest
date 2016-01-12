ac31b66d636ec0878984f30f62d306a5a00f96eb
Luke Plant <L.Plant.98@cantab.net>
2016-01-12 22:20:10 +0000

    Fixed flake8 warnings (and reproducibility)

--- django_functest/__init__.py
+++ django_functest/__init__.py
@@ -1,7 +1,6 @@
-__version__ = '0.1.0'
-
 from .funcwebtest import FuncWebTestMixin
 from .funcselenium import FuncSeleniumMixin
 
+__version__ = '0.1.0'
 
 __all__ = ['FuncWebTestMixin', 'FuncSeleniumMixin']
