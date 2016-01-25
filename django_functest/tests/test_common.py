ebc54a138a10851666a0f5ac208e7aa707fd4da8
Luke Plant <L.Plant.98@cantab.net>
2016-01-25 14:15:53 +0000

    Additional test for `current_url`

--- django_functest/tests/test_common.py
+++ django_functest/tests/test_common.py
@@ -63,6 +63,11 @@ class TestCommonBase(object):
         self.assertRaises(AssertionError, lambda: self.assertTextAbsent("Hello world"))
         self.assertRaises(AssertionError, lambda: self.assertTextAbsent("from me & friends"))
 
+    def test_current_url(self):
+        self.get_url('admin:login')
+        # Check it really is a full URL
+        self.assertTrue(self.current_url.startswith('http'))
+
     def test_is_element_present(self):
         self.get_url('admin:login')
         self.assertTrue(self.is_element_present('#id_username'))
