13ee90d04a730fc59b12eb05890f00b9d1bc5df7
Luke Plant <L.Plant.98@cantab.net>
2016-02-05 10:27:39 +0000

    Bump version: 0.1.1 → 0.1.2-dev

--- django_functest/__init__.py
+++ django_functest/__init__.py
@@ -2,6 +2,6 @@ from .funcselenium import FuncSeleniumMixin
 from .funcwebtest import FuncWebTestMixin
 from .utils import AdminLoginMixin, ShortcutLoginMixin
 
-__version__ = '0.1.1'
+__version__ = '0.1.2-dev'
 
 __all__ = ['FuncWebTestMixin', 'FuncSeleniumMixin', 'ShortcutLoginMixin', 'AdminLoginMixin']
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "0.1.1"
+version = "0.1.2-dev"
 
 if sys.argv[-1] == 'publish':
     try:
