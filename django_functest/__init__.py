7b935b809471b7a9e49bd4cfefd50edde0c10b30
Luke Plant <L.Plant.98@cantab.net>
2016-12-29 08:31:53 +0300

    Bump version: 0.1.9-dev → 0.1.9

--- django_functest/__init__.py
+++ django_functest/__init__.py
@@ -6,6 +6,6 @@ from .funcwebtest import FuncWebTestMixin
 from .utils import AdminLoginMixin, ShortcutLoginMixin
 from .files import Upload
 
-__version__ = '0.1.9-dev'
+__version__ = '0.1.9'
 
 __all__ = ['FuncBaseMixin', 'FuncWebTestMixin', 'FuncSeleniumMixin', 'ShortcutLoginMixin', 'AdminLoginMixin', 'Upload']
