e9e511398881dbf2c9097966fbec984190333f98
Luke Plant <L.Plant.98@cantab.net>
2017-08-16 20:53:25 +0300

    Fixed release script

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@
 # dependency:
 
 python2.7 setup.py bdist_wheel --python-tag=py2 || exit 1
-python3.4 setup.py bdist_wheel --python-tag=py3 || exit 1
+python3.5 setup.py bdist_wheel --python-tag=py3 || exit 1
 
 VERSION=$(./setup.py --version) || exit 1
 twine upload dist/django-functest-$VERSION.tar.gz dist/django_functest-$VERSION-py2-none-any.whl dist/django_functest-$VERSION-py3-none-any.whl || exit 1
