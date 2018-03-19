520da5aaeaefe59f47afe383335fe444e6a25374
Luke Plant <L.Plant.98@cantab.net>
2018-03-19 21:36:46 +0300

    Typo

--- download_firefox.sh
+++ download_firefox.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 wget https://ftp.mozilla.org/pub/firefox/releases/45.2.0esr/firefox-45.2.0esr.linux-x86_64.sdk.tar.bz2
-tar -xzjf firefox-45.2.0esr.linux-x86_64.sdk.tar.bz2
+tar -xvjf firefox-45.2.0esr.linux-x86_64.sdk.tar.bz2
