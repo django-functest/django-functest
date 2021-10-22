24875b6e866914f4b983e5edec686116a6b50503
Luke Plant <L.Plant.98@cantab.net>
2021-10-22 22:01:21 +0300

    Better docstring on Upload class

--- django_functest/files.py
+++ django_functest/files.py
@@ -4,4 +4,12 @@ from webtest import Upload as WebTestUpload
 
 
 class Upload(WebTestUpload):
+    """"
+    A file to upload::
+
+        >>> Upload('filename.txt', b'data')
+        <Upload "filename.txt">
+    """
+    # We deliberately override the docstring of WebTestUpload above,
+    # to provide help that works for Selenium too.
     pass
