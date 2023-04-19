2d4af4a33ba3f4420480e863b20c03e891330887
Luke Plant <L.Plant.98@cantab.net>
2023-04-19 12:44:41 +0100

    Don't check-manifest as pre-commit, it gets in the way sometimes
    Do it as release check instead

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 test $(git rev-parse --abbrev-ref HEAD | tr -d '\n') = 'master' || { echo "Must be on master branch"; exit 1; }
+check-manifest || exit 1
+
 umask 000
 git ls-tree --full-tree --name-only -r HEAD | xargs chmod ugo+r
 python setup.py sdist || exit 1
