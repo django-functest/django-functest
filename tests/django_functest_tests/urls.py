f1098da2361ea7450e7b6c6e48a95c6e8ca2cc53
Luke Plant <L.Plant.98@cantab.net>
2023-04-19 12:46:03 +0100

    Removed some support for old Django versions in tests

--- tests/django_functest_tests/urls.py
+++ tests/django_functest_tests/urls.py
@@ -1,130 +1,34 @@
-import django
 from django.contrib import admin
-
-try:
-    from django.urls import include, path
-
-    url = None
-except ImportError:
-    path = None
-    from django.conf.urls import url, include
+from django.urls import include, path
 
 from . import views
 
-if path is None:
-    urlpatterns = [
-        url(r"^admin/", admin.site.urls),
-        url(r"^django_functest/", include("django_functest.urls")),
-        url(r"^test_misc/", views.test_misc, name="test_misc"),
-        url(
-            r"^redirect_to_misc/",
-            views.redirect_to_misc,
-            name="redirect_to_misc",
-        ),
-        url(
-            r"^set_sess_foo_to_bar/",
-            views.set_sess_foo_to_bar,
-            name="set_sess_foo_to_bar",
-        ),
-        url(r"^list_things/", views.list_things, name="list_things"),
-        url(r"^edit_thing/(?P<thing_id>.*)/", views.edit_thing, name="edit_thing"),
-        url(
-            r"^edit_thing_with_upload/(?P<thing_id>.*)/",
-            views.edit_thing_with_upload,
-            name="edit_thing_with_upload",
-        ),
-        url(
-            r"^thing_cleared/(?P<thing_id>.*)/",
-            views.thing_cleared,
-            name="thing_cleared",
-        ),
-        url(
-            r"^new_browser_session_test/",
-            views.new_browser_session_test,
-            name="new_browser_session_test",
-        ),
-        url(
-            r"^auto_submit_form/",
-            views.auto_submit_form,
-            name="auto_submit_form",
-        ),
-        url(
-            r"^delayed_appearance/",
-            views.delayed_appearance,
-            name="delayed_appearance",
-        ),
-        url(
-            r"^overflowing/",
-            views.overflowing,
-            name="overflowing",
-        ),
-        url(
-            r"^long_page/",
-            views.long_page,
-            name="long_page",
-        ),
-        url(
-            r"with_confirm",
-            views.with_confirm,
-            name="with_confirm",
-        ),
-    ]
-else:
-    urlpatterns = [
-        path("admin/", admin.site.urls),
-        path("django_functest/", include("django_functest.urls")),
-        path("test_misc/", views.test_misc, name="test_misc"),
-        path(
-            "redirect_to_misc/",
-            views.redirect_to_misc,
-            name="redirect_to_misc",
-        ),
-        path(
-            "set_sess_foo_to_bar/",
-            views.set_sess_foo_to_bar,
-            name="set_sess_foo_to_bar",
-        ),
-        path("list_things/", views.list_things, name="list_things"),
-        path("edit_thing/<str:thing_id>/", views.edit_thing, name="edit_thing"),
-        path(
-            "edit_thing_with_upload/<str:thing_id>/",
-            views.edit_thing_with_upload,
-            name="edit_thing_with_upload",
-        ),
-        path("thing_cleared/<str:thing_id>/", views.thing_cleared, name="thing_cleared"),
-        path(
-            "new_browser_session_test/",
-            views.new_browser_session_test,
-            name="new_browser_session_test",
-        ),
-        path(
-            r"auto_submit_form/",
-            views.auto_submit_form,
-            name="auto_submit_form",
-        ),
-        path(
-            r"delayed_appearance/",
-            views.delayed_appearance,
-            name="delayed_appearance",
-        ),
-        path(
-            r"overflowing/",
-            views.overflowing,
-            name="overflowing",
-        ),
-        path(
-            r"long_page/",
-            views.long_page,
-            name="long_page",
-        ),
-        path(
-            r"with_confirm",
-            views.with_confirm,
-            name="with_confirm",
-        ),
-    ]
-
-if django.VERSION < (1, 9):
-    from django.conf.urls import patterns
-
-    urlpatterns = patterns("", *urlpatterns)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("django_functest/", include("django_functest.urls")),
+    path("test_misc/", views.test_misc, name="test_misc"),
+    path(
+        "redirect_to_misc/",
+        views.redirect_to_misc,
+        name="redirect_to_misc",
+    ),
+    path(
+        "set_sess_foo_to_bar/",
+        views.set_sess_foo_to_bar,
+        name="set_sess_foo_to_bar",
+    ),
+    path("list_things/", views.list_things, name="list_things"),
+    path("edit_thing/<str:thing_id>/", views.edit_thing, name="edit_thing"),
+    path(
+        "edit_thing_with_upload/<str:thing_id>/",
+        views.edit_thing_with_upload,
+        name="edit_thing_with_upload",
+    ),
+    path("thing_cleared/<str:thing_id>/", views.thing_cleared, name="thing_cleared"),
+    path("new_browser_session_test/", views.new_browser_session_test, name="new_browser_session_test"),
+    path(r"auto_submit_form/", views.auto_submit_form, name="auto_submit_form"),
+    path(r"delayed_appearance/", views.delayed_appearance, name="delayed_appearance"),
+    path(r"overflowing/", views.overflowing, name="overflowing"),
+    path(r"long_page/", views.long_page, name="long_page"),
+    path(r"with_confirm", views.with_confirm, name="with_confirm"),
+]
