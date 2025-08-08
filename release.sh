fe7660d57302e5d2b6d50cd507a96b92c2a931de
Luke Plant <luke@lukeplant.me.uk>
2025-08-08 09:36:14 +0100

    Fix release.sh

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ umask 000
 rm -rf build dist
 git ls-tree --full-tree --name-only -r HEAD | xargs chmod ugo+r
 uv build --sdist --wheel || exit 1
-uv publish --sdist --wheel  || exit 1
+uv publish || exit 1
 
 VERSION=$(uv pip show django-functest | grep 'Version: ' | cut -f 2 -d ' ' | tr -d '\n') || exit 1
 
